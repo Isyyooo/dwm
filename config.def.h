ccd7267df97c180e4701e30ed0ef09b2714586e1
yyooo <yyooo1216@gmail.com>
2022-12-16 14:30:54 +0800

    add some wm rule

--- config.def.h
+++ config.def.h
@@ -37,8 +37,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-  {"Pavucontrol", NULL,      NULL,      0,            1,           -1 },
+	/* class        instance    title       tags mask     isfloating   monitor */
+  {"Pavucontrol", NULL,       NULL,       0,            1,           -1 },
+  {"Nemo",        NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
